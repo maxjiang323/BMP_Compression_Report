00e036fccb9b5502f0969a178a306a03802da10d
maxjiang323 <maxjiang.323@gmail.com>
2025-01-19 22:43:46 +0800

    2D-DCT,2D-IDCT,(un-)Quantization

---
+++ mathCalculation.h
@@ -0,0 +1,16 @@
+#ifndef MATHCALCULATION_H
+#define MATHCALCULATION_H
+
+#include <math.h>
+#include "definitions.h"
+
+// encoder.c
+void generate_basis_vector(float basis_vector[H][W][H][W]);
+void compute_2D_DCT(int height, int width, float **f, float ****F, float basis_vector[H][W][H][W]);
+void quantization(int height, int width, short *****qF, float ****F, char channel);
+
+// decoder.c
+void un_quantization(int height, int width, short ****qF, float *****F, char channel);
+void compute_2D_IDCT(int height, int width, float **f, float ****F, float basis_vector[H][W][H][W]);
+
+#endif // MATHCALCULATION_H
